7135cbdd73ad5161dbdeec04ea2aa699ad2729b0
Luis Paz <lpaz8806@gmail.com>
2022-10-13 15:12:08 +0200

    Update Program.cs

--- L02FlowControl/Program.cs
+++ L02FlowControl/Program.cs
@@ -1,6 +1,9 @@
 ﻿
 // read two numbers from the user
 // print the maximum
+
+using System.Threading.Channels;
+
 void RunMaxOfTwoNumbers()
 {
     var n1 = int.Parse(Console.ReadLine());
@@ -97,5 +100,80 @@ void RunPrintSumOf5Numbers()
 
 void RunReadUntilQuit()
 {
+    while (true)
+    {
+        var input = Console.ReadLine().Trim();
+        
+        if ("quit" == input)
+            break;  // exit the loop immediately
+        
+        if("hello" == input)
+            continue;   // restart the loop
+        
+        Console.WriteLine("Keep writing, you fool!");
+    }
+}
+
+// Tell the user to guess a number
+// if the number is the one you "thought", congratulate him
+// otherwise let him to try again
+void RunGuessNumber()
+{
+    var random = 6;
+
+    while (true)
+    {
+        Console.Write("Guess a number, you freak: ");
+        var number = int.Parse(Console.ReadLine());
+        if (number == random)
+            break;
+    }
     
+    Console.WriteLine("Congratulations. You've read my mind");
+}
+void RunGuessNumberDoWhile()
+{
+    var random = 6;
+    var number = 0;
+    
+    do
+    {
+        Console.Write("Guess a number, you freak: ");
+        number = int.Parse(Console.ReadLine());
+    } while (number != random);
+    
+    Console.WriteLine("Congratulations. You've read my mind");
+}
+
+void RunMultiplicationTable()
+{
+    int foo = 12;
+    for (int i = 1; i <= 9; i++)
+    {
+        Console.WriteLine($"Multiplication table for {i}");
+        for (int j = 1; j <= 9; j++)
+        {
+            Console.WriteLine($"{i} * {j} = {i * j}");
+        }
+        
+        Console.WriteLine();
+    }
+}
+
+int Sum(int x, int y)
+{
+    return x + y;
+}
+int Max(int x, int y)
+{
+    var foo = 1;
+    return x > y ? x : y;
+}
+int Square(int x)
+{
+    return x * x;
+}
+bool IsEven(int x)
+{
+    return x % 2 == 0;
 }
